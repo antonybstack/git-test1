486a81798e3d628006296649093f40bcee026d1c
antonybstack <ablyakhe@uncc.edu>
2020-09-17 19:29:40 -0400

    reorganize files to include each week folders

---
+++ week02/README.md
@@ -0,0 +1,6 @@
+161.35.126.158
+
+ESC
+:set paste
+press i
+SHIFT + Insert (with a text copied on your clipboard)
---
+++ week02/my-server.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.write("Hello World from Antony Blyakher and NodeJS!");
+    res.end();
+  })
+  .listen(3000, function () {
+    console.log("Server running at port 3000");
+  });
